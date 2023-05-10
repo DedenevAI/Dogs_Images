fc8c79dd843e1d1843c35bb72916836f987f1eb1
alexMac <dedenevai3263@yandex.ru>
2023-05-10 22:28:49 +0300

    ViewModel added

--- app/src/main/java/com/example/dogsimages/MainActivity.java
+++ app/src/main/java/com/example/dogsimages/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.dogsimages;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private MainViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.getDogImageMutableLiveData().observe(this, new Observer<DogImage>() {
+            @Override
+            public void onChanged(DogImage dogImage) {
+                Log.d(TAG, dogImage.toString());
+            }
+        });
+
+        viewModel.loadDocImage();
     }
+
 }
